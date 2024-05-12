a427b3754566d362ef7d8bb6a7586cd8c3bc97db
Yehye2 <133616786+Yehye2@users.noreply.github.com>
2024-05-13 06:06:42 +0900

    456

---
+++ script.js
@@ -0,0 +1,13 @@
+// Swiper 초기화
+var swiper = new Swiper('.guild-slider', {
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
